4098d9c9dee4a8b4b3a126a88e23f0ecd3977bed
ufcyg <mellowski@gmail.com>
2021-03-05 13:29:31 +0100

    minor

--- src/ASProductBundles.php
+++ src/ASProductBundles.php
@@ -3,7 +3,46 @@
 namespace ASProductBundles;
 
 use Shopware\Core\Framework\Plugin;
+use Shopware\Core\Framework\Plugin\Context\ActivateContext;
+use Shopware\Core\Framework\Plugin\Context\DeactivateContext;
+use Shopware\Core\Framework\Plugin\Context\InstallContext;
+use Shopware\Core\Framework\Plugin\Context\UninstallContext;
+use Shopware\Core\Framework\Plugin\Context\UpdateContext;
 
 class ASProductBundles extends Plugin
 {
+    /** @inheritDoc */
+    public function install(InstallContext $installContext): void
+    {
+    }
+
+    /** @inheritDoc */
+    public function postInstall(InstallContext $installContext): void
+    {
+    }
+
+    /** @inheritDoc */
+    public function update(UpdateContext $updateContext): void
+    {
+    }
+
+    /** @inheritDoc */
+    public function postUpdate(UpdateContext $updateContext): void
+    {
+    }
+
+    /** @inheritDoc */
+    public function activate(ActivateContext $activateContext): void
+    {
+    }
+
+    /** @inheritDoc */
+    public function deactivate(DeactivateContext $deactivateContext): void
+    {
+    }
+
+    /** @inheritDoc */
+    public function uninstall(UninstallContext $context): void
+    {
+    }
 }
